09373c69a681e8eb02276de227643fcf44710685
Raghav Sood <raghavsood@appaholics.in>
2018-03-18 14:11:21 +1300

    Address encoding complete

--- addrconv_test.go
+++ addrconv_test.go
@@ -39,3 +39,85 @@ func TestDigibyteAddress(t *testing.T) {
 		t.Errorf("Incorrect address. Expected %s, got %s", "DCXiSSQwi7gw9YXrMY4mxt2i4hQZEBb5Yv", address)
 	}
 }
+
+func TestBTCP2SHAddress(t *testing.T) {
+	var script blockutils.Script
+	var err error
+	script, err = hex.DecodeString("a9144aef67ed61d391d6f3d9903ead92386c1efc992587")
+	if err != nil {
+		t.Errorf("Error decoding hex: %s", err)
+	}
+	address, err := ToNetworkAddress(script, BitcoinNetwork)
+	if err != nil {
+		t.Errorf("Error encoding address: %s", err)
+	}
+
+	if address != "38XEixUj1QpcqxTWbxvqdbv4Mjre4imw9Z" {
+		t.Errorf("Incorrect address. Expected %s, got %s", "38XEixUj1QpcqxTWbxvqdbv4Mjre4imw9Z", address)
+	}
+}
+
+func TestOmniOpReturnScript(t *testing.T) {
+	var script blockutils.Script
+	var err error
+	script, err = hex.DecodeString("6a146f6d6e69000000000000000300000000000000c8")
+	if err != nil {
+		t.Errorf("Error decoding hex: %s", err)
+	}
+	address, err := ToNetworkAddress(script, BitcoinNetwork)
+	if err != nil {
+		t.Errorf("Error encoding address: %s", err)
+	}
+
+	if address != "6a146f6d6e69000000000000000300000000000000c8" {
+		t.Errorf("Incorrect address. Expected %s, got %s", "6a146f6d6e69000000000000000300000000000000c8", address)
+	}
+}
+
+func TestBech32Addresses(t *testing.T) {
+	var script blockutils.Script
+	var err error
+
+	var scripts = []string{"0014751e76e8199196d454941c45d1b3a323f1433bd6", "0020701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d", "00203eb5062a0b0850b23a599425289a091c374ca934101d03144f060c5b46a979be"}
+	var addresses = []string{"bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4", "bc1qwqdg6squsna38e46795at95yu9atm8azzmyvckulcc7kytlcckxswvvzej", "bc1q866sv2stppgtywjejsjj3xsfrsm5e2f5zqwsx9z0qcx9k34f0xlqaslnwr"}
+
+	for i, v := range scripts {
+
+		script, err = hex.DecodeString(v)
+		if err != nil {
+			t.Errorf("Error decoding hex: %s", err)
+		}
+		address, err := ToNetworkAddress(script, BitcoinNetwork)
+		if err != nil {
+			t.Errorf("Error encoding address: %s", err)
+		}
+
+		if address != addresses[i] {
+			t.Errorf("Incorrect address. Expected %s, got %s", addresses[i], address)
+		}
+	}
+}
+
+func TestBTCP2PK(t *testing.T) {
+	var script blockutils.Script
+	var err error
+
+	var scripts = []string{"4104f601d3111e0f502f8d5927fd4077e8723f9b156138a776afa5ceae4d6da7370d4f1effb4171cd8ce7b40d54e3a0b45b528575ce63465986810085babdef06f01ac", "2103f601d3111e0f502f8d5927fd4077e8723f9b156138a776afa5ceae4d6da7370dac", "410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac"}
+	var addresses = []string{"18AuG6THx5V9JuZbtWtrabYWfxY28YcXhN", "1ABTTFaiYdTNX4yzQ1vn5QP1URrHwCDbFc", "12cbQLTFMXRnSzktFkuoG3eHoMeFtpTu3S"}
+
+	for i, v := range scripts {
+
+		script, err = hex.DecodeString(v)
+		if err != nil {
+			t.Errorf("Error decoding hex: %s", err)
+		}
+		address, err := ToNetworkAddress(script, BitcoinNetwork)
+		if err != nil {
+			t.Errorf("Error encoding address: %s", err)
+		}
+
+		if address != addresses[i] {
+			t.Errorf("Incorrect address. Expected %s, got %s", addresses[i], address)
+		}
+	}
+}
--- encoder.go
+++ encoder.go
@@ -1,17 +1,59 @@
 package addrconv
 
 import (
+	"github.com/RaghavSood/addrconv/bech32"
 	"github.com/RaghavSood/blockutils"
 )
 
 func (network Network) Encode(script blockutils.Script) (string, error) {
+	if script.IsP2PK() {
+		hash160, err := script.P2PKHash160()
+		if err != nil {
+			return script.String(), err
+		}
+		return CheckEncode(hash160, network.PubKeyPrefix), nil
+	}
+
 	if script.IsP2PKH() {
 		hash160, err := script.P2PKHHash160()
 		if err != nil {
-			return "", err
+			return script.String(), err
 		}
-
 		return CheckEncode(hash160, network.PubKeyPrefix), nil
 	}
-	return "test", nil
+
+	if script.IsP2SH() {
+		hash160, err := script.P2SHHash160()
+		if err != nil {
+			return script.String(), err
+		}
+		return CheckEncode(hash160, network.ScriptHashPrefix), nil
+	}
+
+	if script.IsWitnessScript() {
+		witnessVersion, err := script.WitnessVersion()
+		if err != nil {
+			return script.String(), err
+		}
+		intWitnessVersion := int(witnessVersion)
+		witnessProgram, err := script.WitnessProgram()
+		if err != nil {
+			return script.String(), err
+		}
+		intWitnessProgram, err := toIntSlice(witnessProgram)
+		if err != nil {
+			return script.String(), err
+		}
+		return bech32.SegwitAddrEncode(network.Bech32Prefix, intWitnessVersion, intWitnessProgram)
+	}
+
+	return script.String(), nil
+}
+
+func toIntSlice(buf []byte) ([]int, error) {
+	vals := make([]int, len(buf))
+	for i := 0; i < len(vals); i++ {
+		vals[i] = int(buf[i])
+	}
+	return vals, nil
 }
